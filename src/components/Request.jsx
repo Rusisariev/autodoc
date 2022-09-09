b95b8c86f0f8917fc4a58b36d8218c6f7949953f
Ruslan Sariev <rusisariev@gmail.com>
2022-09-09 09:11:36 +0600

    init

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -31,6 +31,7 @@ const Request = () => {
     })
     const [companies, setCompanies] = useState([])
     const [products, setProducts] = useState([])
+    const [al, setAl] = useState(false)
 
     const handlerChange = (e) => {
         const {name, value} = e.target
@@ -82,6 +83,7 @@ const Request = () => {
                 productCategory: "",
                 status: true
             })
+            setAl(true)
             return res
         })
     }
@@ -103,124 +105,134 @@ const Request = () => {
     }
 
     return (
-        <form onSubmit={handlerClick}>
-            <div className="row mt-3 align-items-center">
-                <p className="mb-0 col-4">Валюта:</p>
-                <div className="col-8">
-                    <select className="form-select" aria-label="Default select example" name="currency" value={requestState.currency} onChange={handlerChange} required>
-                        <option defaultValue>...</option>
-                        {
-                            currency.map((item, idx) => {
-                                return <option value={item.title} key={idx}>{item.title}</option>
-                            })
-                        }
-                    </select>
+        <>
+            <form onSubmit={handlerClick}>
+                <div className="row mt-3 align-items-center">
+                    <p className="mb-0 col-4">Валюта:</p>
+                    <div className="col-8">
+                        <select className="form-select" aria-label="Default select example" name="currency" value={requestState.currency} onChange={handlerChange} required>
+                            <option defaultValue>...</option>
+                            {
+                                currency.map((item, idx) => {
+                                    return <option value={item.title} key={idx}>{item.title}</option>
+                                })
+                            }
+                        </select>
+                    </div>
+                </div>
+                <div className="row mt-3 align-items-center">
+                    <p className="mb-0 col-4">Цена:</p>
+                    <div className="col-8">
+                        <input type="number" className="form-control" min="0" name="price" value={requestState.price} onChange={handlerChange} required/>
+                    </div>
+                </div>
+                <div className="row mt-3 align-items-center">
+                    <p className="mb-0 col-4">Товар или услуга:</p>
+                    <div className="col-8">
+                        <select className="form-select" aria-label="Default select example" name="productOrService" value={requestState.productOrService} onChange={handlerChange} required>
+                            <option defaultValue>...</option>
+                            <option value="Product">Product</option>
+                            <option value="Service">Service</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="row mt-3 align-items-center">
+                    <p className="mb-0 col-4">Продать или купить:</p>
+                    <div className="col-8">
+                        <select className="form-select" aria-label="Default select example" name="sellOrBuy" value={requestState.sellOrBuy} onChange={handlerChange} required>
+                            <option defaultValue>...</option>
+                            <option value="Sell">Sell</option>
+                            <option value="Buy">Buy</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="row mt-3 align-items-center">
+                    <p className="mb-0 col-4">Наша компания:</p>
+                    <div className="col-8">
+                        <select className="form-select" aria-label="Default select example" name="ourCompany" value={requestState.ourCompany} onChange={handlerChange} required>
+                            <option defaultValue>...</option>
+                            {
+                                companies?.map((item, idx) => (
+                                    <option value={item.id} key={idx}>
+                                        {item.company_short_name_en}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                </div>
+                <div className="row mt-3 align-items-center">
+                    <p className="mb-0 col-4">Банк нашей компании:</p>
+                    <div className="col-8">
+                        <select className="form-select" aria-label="Default select example" name="bankOfOurCompany" value={requestState.bankOfOurCompany} onChange={handlerChange} required>
+                            <option defaultValue>...</option>
+                            {
+                                companies?.filter(el => el.id === Number(requestState.ourCompany))[0]?.banks?.map((item, idx) => (
+                                    <option key={idx} value={item.id}>{item.company_bank_name_en}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                </div>
+                <div className="row mt-3 align-items-center">
+                    <p className="mb-0 col-4">Компания контрагента *:</p>
+                    <div className="col-8">
+                        <select className="form-select" aria-label="Default select example" name="counterPartyCompany" value={requestState.counterPartyCompany} onChange={handlerChange} required>
+                            <option defaultValue>...</option>
+                            {
+                                companies?.map((item, idx) => (
+                                    <option value={item.id} key={idx}>
+                                        {item.company_short_name_en}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                </div>
+                <div className="row mt-3 align-items-center">
+                    <p className="mb-0 col-4">Банк контрагента:</p>
+                    <div className="col-8">
+                        <select className="form-select" aria-label="Default select example" name="counterPartyBank" value={requestState.counterPartyBank} onChange={handlerChange} required>
+                            <option defaultValue>...</option>
+                            {
+                                companies?.filter(el => el.id === Number(requestState.counterPartyCompany))[0]?.banks?.map((item, idx) => (
+                                    <option key={idx} value={item.id}>{item.company_bank_name_en}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                </div>
+                <div className="row mt-3 align-items-center">
+                    <p className="mb-0 col-4">Категория продукта:</p>
+                    <div className="col-8">
+                        <select className="form-select" aria-label="Default select example" name="productCategory" value={requestState.productCategory} onChange={handlerChange} required>
+                            <option defaultValue>...</option>
+                            {
+                                products?.map((item, idx) => (
+                                    <option value={item.id} key={idx}>
+                                        {item.name}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                </div>
+                <div className='d-flex justify-content-end mb-5 mt-3 align-items-center'>
+                    <button className="btn btn-danger me-3" onClick={clearState}>Сбросить</button>
+                    <button className="btn btn-primary" type="submit">Отправить</button>
+                </div>
+            </form>
+            <div className={al ? "alert-custom" : "alert-custom hidden"}>
+                <div className="alert alert-success d-flex align-items-center">
+                    <i className="bi bi-exclamation-circle flex-shrink-0 me-2"/>
+                    <div>
+                        Успешно!!!
+                    </div>
+                    <button type="button" className="btn-close ms-5" onClick={() => setAl(false)}/>
                 </div>
             </div>
-            <div className="row mt-3 align-items-center">
-                <p className="mb-0 col-4">Цена:</p>
-                <div className="col-8">
-                    <input type="number" className="form-control" min="0" name="price" value={requestState.price} onChange={handlerChange} required/>
-                </div>
-            </div>
-            <div className="row mt-3 align-items-center">
-                <p className="mb-0 col-4">Товар или услуга:</p>
-                <div className="col-8">
-                    <select className="form-select" aria-label="Default select example" name="productOrService" value={requestState.productOrService} onChange={handlerChange} required>
-                        <option defaultValue>...</option>
-                        <option value="Product">Product</option>
-                        <option value="Service">Service</option>
-                    </select>
-                </div>
-            </div>
-            <div className="row mt-3 align-items-center">
-                <p className="mb-0 col-4">Продать или купить:</p>
-                <div className="col-8">
-                    <select className="form-select" aria-label="Default select example" name="sellOrBuy" value={requestState.sellOrBuy} onChange={handlerChange} required>
-                        <option defaultValue>...</option>
-                        <option value="Sell">Sell</option>
-                        <option value="Buy">Buy</option>
-                    </select>
-                </div>
-            </div>
-            <div className="row mt-3 align-items-center">
-                <p className="mb-0 col-4">Наша компания:</p>
-                <div className="col-8">
-                    <select className="form-select" aria-label="Default select example" name="ourCompany" value={requestState.ourCompany} onChange={handlerChange} required>
-                        <option defaultValue>...</option>
-                        {
-                            companies?.map((item, idx) => (
-                                <option value={item.id} key={idx}>
-                                    {item.company_short_name_en}
-                                </option>
-                            ))
-                        }
-                    </select>
-                </div>
-            </div>
-            <div className="row mt-3 align-items-center">
-                <p className="mb-0 col-4">Банк нашей компании:</p>
-                <div className="col-8">
-                    <select className="form-select" aria-label="Default select example" name="bankOfOurCompany" value={requestState.bankOfOurCompany} onChange={handlerChange} required>
-                        <option defaultValue>...</option>
-                        {
-                            companies?.filter(el => el.id === Number(requestState.ourCompany))[0]?.banks?.map((item, idx) => (
-                                <option key={idx} value={item.id}>{item.company_bank_name_en}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-            </div>
-            <div className="row mt-3 align-items-center">
-                <p className="mb-0 col-4">Компания контрагента *:</p>
-                <div className="col-8">
-                    <select className="form-select" aria-label="Default select example" name="counterPartyCompany" value={requestState.counterPartyCompany} onChange={handlerChange} required>
-                        <option defaultValue>...</option>
-                        {
-                            companies?.map((item, idx) => (
-                                <option value={item.id} key={idx}>
-                                    {item.company_short_name_en}
-                                </option>
-                            ))
-                        }
-                    </select>
-                </div>
-            </div>
-            <div className="row mt-3 align-items-center">
-                <p className="mb-0 col-4">Банк контрагента:</p>
-                <div className="col-8">
-                    <select className="form-select" aria-label="Default select example" name="counterPartyBank" value={requestState.counterPartyBank} onChange={handlerChange} required>
-                        <option defaultValue>...</option>
-                        {
-                            companies?.filter(el => el.id === Number(requestState.counterPartyCompany))[0]?.banks?.map((item, idx) => (
-                                <option key={idx} value={item.id}>{item.company_bank_name_en}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-            </div>
-            <div className="row mt-3 align-items-center">
-                <p className="mb-0 col-4">Категория продукта:</p>
-                <div className="col-8">
-                    <select className="form-select" aria-label="Default select example" name="productCategory" value={requestState.productCategory} onChange={handlerChange} required>
-                        <option defaultValue>...</option>
-                        {
-                            products?.map((item, idx) => (
-                                <option value={item.id} key={idx}>
-                                    {item.name}
-                                </option>
-                            ))
-                        }
-                    </select>
-                </div>
-            </div>
-            <div className='d-flex justify-content-end mb-5 mt-3 align-items-center'>
-                <button className="btn btn-danger me-3" onClick={clearState}>Сбросить</button>
-                <button className="btn btn-primary" type="submit">Отправить</button>
-            </div>
-        </form>
-)
-    ;
+        </>
+    )
 };
 
 export default Request;
