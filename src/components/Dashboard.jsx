cc650d72e01a41f83416c6b0855071a7b60adb9d
Ruslan Sariev <rusisariev@gmail.com>
2022-10-17 00:11:02 +0600

    init

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,6 +5,8 @@ import axiosSSR from "../axios";
 const Dashboard = () => {
     const [dashboard, setDashboard] = useState([]);
     const [superUser, setSuperUser] = useState(false);
+    const [modal, setModal] = useState(false);
+    const [details, setDetails] = useState(0);
 
     const patchDashboard = async (id, status) => {
         const res = await axiosSSR.patch(`/api/inner_traids_dashboard/${id}/`, {
@@ -27,9 +29,14 @@ const Dashboard = () => {
         }
     };
 
+    const getDetail = async (id) => {
+        const res = await axiosSSR.get(`/api/inner_traids_dashboard/${id}`);
+        setDetails(res.data);
+    };
+
     useEffect(() => {
         getDashboard();
-        if(window.localStorage.getItem("token")){
+        if (window.localStorage.getItem("token")) {
             getUser();
         }
     }, []);
@@ -41,33 +48,40 @@ const Dashboard = () => {
                 {dashboard
                     .filter((el) => el.status === "Todo")
                     .map((item) => (
-                        <div className="card" key={item.id}>
+                        <div
+                            className="card"
+                            key={item.id}
+                            onClick={() => {
+                                getDetail(item.id);
+                                setModal(true);
+                            }}
+                        >
                             <div className="d-flex flex-column justify-content-between">
                                 <p className="mb-0">
-                                    {item.buyer_company_name}, {item.seller_company_name}
+                                    {item.buyer_company_name},{" "}
+                                    {item.seller_company_name}
                                 </p>
-                                <a href={item.inner_traid} download>
-                                    Скачать файл
-                                </a>
                             </div>
                             {superUser ? (
                                 <div className="d-flex mt-2">
                                     <button
                                         className="btn btn-primary me-2"
-                                        onClick={() =>
+                                        onClick={(e) => {
+                                            e.stopPropagation();
                                             patchDashboard(
                                                 item.id,
                                                 "In progress"
-                                            )
-                                        }
+                                            );
+                                        }}
                                     >
                                         В процессе
                                     </button>
                                     <button
                                         className="btn btn-primary"
-                                        onClick={() =>
-                                            patchDashboard(item.id, "Done")
-                                        }
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            patchDashboard(item.id, "Done");
+                                        }}
                                     >
                                         В готово
                                     </button>
@@ -81,30 +95,37 @@ const Dashboard = () => {
                 {dashboard
                     .filter((el) => el.status === "In progress")
                     .map((item) => (
-                        <div className="card" key={item.id}>
+                        <div
+                            className="card"
+                            key={item.id}
+                            onClick={() => {
+                                getDetail(item.id);
+                                setModal(true);
+                            }}
+                        >
                             <div className="d-flex flex-column justify-content-between">
                                 <p className="mb-0">
-                                    {item.buyer_company_name}, {item.seller_company_name}
+                                    {item.buyer_company_name},{" "}
+                                    {item.seller_company_name}
                                 </p>
-                                <a href={item.inner_traid} download>
-                                    Скачать файл
-                                </a>
                             </div>
                             {superUser ? (
                                 <div className="d-flex mt-2">
-                                     <button
+                                    <button
                                         className="btn btn-primary me-2"
-                                        onClick={() =>
-                                            patchDashboard(item.id, "Todo")
-                                        }
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            patchDashboard(item.id, "Todo");
+                                        }}
                                     >
                                         К выполнению
                                     </button>
                                     <button
                                         className="btn btn-primary"
-                                        onClick={() =>
-                                            patchDashboard(item.id, "Done")
-                                        }
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            patchDashboard(item.id, "Done");
+                                        }}
                                     >
                                         В готово
                                     </button>
@@ -118,33 +139,40 @@ const Dashboard = () => {
                 {dashboard
                     .filter((el) => el.status === "Done")
                     .map((item) => (
-                        <div className="card" key={item.id}>
+                        <div
+                            className="card"
+                            key={item.id}
+                            onClick={() => {
+                                getDetail(item.id);
+                                setModal(true);
+                            }}
+                        >
                             <div className="d-flex flex-column justify-content-between">
                                 <p className="mb-0">
-                                    {item.buyer_company_name}, {item.seller_company_name}
+                                    {item.buyer_company_name},{" "}
+                                    {item.seller_company_name}
                                 </p>
-                                <a href={item.inner_traid} download>
-                                    Скачать файл
-                                </a>
                             </div>
                             {superUser ? (
                                 <div className="d-flex mt-2">
                                     <button
                                         className="btn btn-primary me-2"
-                                        onClick={() =>
-                                            patchDashboard(item.id, "Todo")
-                                        }
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            patchDashboard(item.id, "Todo");
+                                        }}
                                     >
                                         К выполнению
                                     </button>
                                     <button
                                         className="btn btn-primary me-2"
-                                        onClick={() =>
+                                        onClick={(e) => {
+                                            e.stopPropagation();
                                             patchDashboard(
                                                 item.id,
                                                 "In progress"
-                                            )
-                                        }
+                                            );
+                                        }}
                                     >
                                         В процессе
                                     </button>
@@ -152,6 +180,31 @@ const Dashboard = () => {
                             ) : null}
                         </div>
                     ))}
+            </div>
+            <div
+                className="dashboard-modal"
+                style={!modal ? { display: "none" } : { display: "flex" }}
+                onClick={() => setModal(false)}
+            >
+                <div
+                    className="dashboard-modal-content"
+                    onClick={(e) => e.stopPropagation()}
+                >
+                    <p>Номер заказа: {details?.deal_number}</p>
+                    <p>Покупатель: {details?.buyer_company_name}</p>
+                    <p>Продовец: {details?.seller_company_name}</p>
+                    <p>
+                        <a href={details?.inner_traid} className="mb-2">
+                            Cкачать файл
+                        </a>
+                    </p>
+                    <div className="d-flex justify-content-between">
+                        <p className="mb-0">
+                            Сумма: {details?.amount_of_payment}
+                        </p>
+                        <p className="mb-0">{details?.date}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
